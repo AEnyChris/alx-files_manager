5216b7e22215958f0db4fa20908be0a519268ade
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-26 09:08:54 +0100

    lint correction

--- utils/db.js
+++ utils/db.js
@@ -2,26 +2,28 @@ const { MongoClient } = require('mongodb');
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost'
-    this.port = process.env.DB_PORT || 27017
-    this.database = process.env.DB_DATABASE || 'files_manager'
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
     this.client = new MongoClient(`mongodb://${this.host}:${this.port}/${this.database}`, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.client.connect()
+    this.client.connect();
   }
 
-  isAlive = () => this.client.isConnected();
-
-  nbUsers = () => {
-    const usersCollection = this.client.db().collection('users')
-    return usersCollection.countDocuments({}, { hint: "_id_" });
+  isAlive() {
+    return this.client.isConnected();
   }
 
-  nbFiles = () => {
-    const filesCollection = this.client.db().collection('files')
+  nbUsers() {
+    const usersCollection = this.client.db().collection('users');
+    return usersCollection.countDocuments({}, { hint: '_id_' });
+  }
+
+  nbFiles() {
+    const filesCollection = this.client.db().collection('files');
     // console.log(filesCollection.find({}).toArray());
-    return filesCollection.countDocuments({}, { hint: "_id_" });
+    return filesCollection.countDocuments({}, { hint: '_id_' });
   }
 }
 
-const dbClient =  new DBClient();
+const dbClient = new DBClient();
 export default dbClient;
